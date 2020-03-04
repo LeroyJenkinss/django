dbce8ff3715b285abeb92c60a6d68abacaa345bf
Tarik <tarik@integrationpartners.nl>
2020-03-04 10:11:37 +0100

    chiars

---
+++ CinemaProject/Model/Repository/ChairsRepository.cs
@@ -0,0 +1,37 @@
+﻿using CinemaProject.View;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace CinemaProject.Model.Repository
+{
+    class ChairsRepository
+    {
+        public List<ChairView> GetAll()
+        {
+            var pathToJsonFile = GetPathToJson();
+
+            List<ChairView> items;
+
+            using (StreamReader r = new StreamReader(pathToJsonFile))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<ChairView>>(json);
+
+            }
+            return items;
+        }
+        public string GetPathToJson()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            var pathToJsonFile = Directory.GetParent(workingDirectory).Parent.FullName + @"\Data\AllMovies.json";
+
+            return pathToJsonFile;
+        }
+    }
+}
