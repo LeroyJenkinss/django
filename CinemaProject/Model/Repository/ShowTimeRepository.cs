a4357c55ecf3d7e660b463b81dd825b34b4bf1c2
Davinci Lisapaly <davincilisapaly@visionplanner.com>
2020-03-16 18:37:20 +0100

    start backend Add Chair Selection

---
+++ CinemaProject/Model/Repository/ShowTimeRepository.cs
@@ -0,0 +1,46 @@
+﻿using CinemaProject.View;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace CinemaProject.Model.Repository
+{
+    public class ShowTimeRepository 
+    {
+        public List<ShowTimeView> GetAll()
+        {
+            var pathToJsonFile = GetPathToJson();
+
+            List<ShowTimeView> items;
+
+            using (StreamReader r = new StreamReader(pathToJsonFile))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<ShowTimeView>>(json);
+            }
+
+            return items;
+        }
+
+        public void AddTakenChairs(int showTimeId, Array takenChairs)
+        {
+
+        }
+
+        public string GetPathToJson()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            var pathToJsonFile = Directory.GetParent(workingDirectory).Parent.FullName + @"\Data\AllMovies.json";
+
+            return pathToJsonFile;
+        }
+    }
+
+
+
+}
---
+++ CinemaProject/Objects/ShowtimeView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaProject.View
+{
+    public class ShowTimeView
+    {
+        public int Id_Showtime;
+        public string Name;
+        public DateTime ReleaseDate;
+        public string Genre;
+
+        public MovieView(
+            int id,
+            string name,
+            string genre,
+            DateTime releaseDate)
+        {
+            Id = id;
+            Name = name;
+            Genre = genre;
+            ReleaseDate = releaseDate;
+        }
+    }
+}
