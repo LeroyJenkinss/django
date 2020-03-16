2bccf7d55f0b68e54f6f4984867994fecc2130ec
Davinci Lisapaly <davincilisapaly@visionplanner.com>
2020-03-16 19:42:44 +0100

    Added new form

---
+++ CinemaProject/Model/Forms/ReservationForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace CinemaProject.Model.Forms
+{
+    partial class ReservationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numberOfPeople = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.person1 = new System.Windows.Forms.ComboBox();
+            this.person2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lastname";
+            // 
+            // numberOfPeople
+            // 
+            this.numberOfPeople.FormattingEnabled = true;
+            this.numberOfPeople.Location = new System.Drawing.Point(312, 91);
+            this.numberOfPeople.Name = "numberOfPeople";
+            this.numberOfPeople.Size = new System.Drawing.Size(121, 21);
+            this.numberOfPeople.TabIndex = 2;
+            this.numberOfPeople.SelectedIndexChanged += new System.EventHandler(this.numberOfPeople_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Number of guests to make an reservation";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Seat 1";
+            // 
+            // person1
+            // 
+            this.person1.FormattingEnabled = true;
+            this.person1.Location = new System.Drawing.Point(312, 142);
+            this.person1.Name = "person1";
+            this.person1.Size = new System.Drawing.Size(121, 21);
+            this.person1.TabIndex = 5;
+            this.person1.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // person2
+            // 
+            this.person2.FormattingEnabled = true;
+            this.person2.Location = new System.Drawing.Point(312, 179);
+            this.person2.Name = "person2";
+            this.person2.Size = new System.Drawing.Size(121, 21);
+            this.person2.TabIndex = 8;
+            this.person2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Seat 2";
+            // 
+            // ReservationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.person2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.person1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numberOfPeople);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ReservationForm";
+            this.Text = "ReservationForm";
+            this.Load += new System.EventHandler(this.ReservationForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox numberOfPeople;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox person1;
+        private System.Windows.Forms.ComboBox person2;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ CinemaProject/Model/Forms/ReservationForm.cs
@@ -0,0 +1,88 @@
+﻿using CinemaProject.Model.Repository;
+using CinemaProject.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CinemaProject.Model.Forms
+{
+    public partial class ReservationForm : Form
+    {
+        private readonly ShowTimeRepository _showTimeRepository;
+
+        public ReservationForm()
+        {
+            InitializeComponent();
+            _showTimeRepository = new ShowTimeRepository();
+        }
+
+        private void ReservationForm_Load(object sender, EventArgs e)
+        {
+            var allShowtimes = _showTimeRepository.GetAll();
+            person1.Visible = false;
+            person2.Visible = false;
+
+            foreach (ShowTimeView i in allShowtimes)
+            {
+                
+                
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numberOfPeople_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var reservationQuantity = int.Parse(numberOfPeople.Text);
+            var counter = 0;
+            while (counter <= reservationQuantity)
+            {
+                switch (reservationQuantity)
+                {
+                    case 1:
+                        person1.Visible = true;
+                        break;
+                    case 2:
+                        person2.Visible = true;
+                        break;
+                    case 3:
+                        person2.Visible = true;
+                        break;
+                    case 4:
+                        person2.Visible = true;
+                        break;
+                    case 5:
+                        person2.Visible = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
