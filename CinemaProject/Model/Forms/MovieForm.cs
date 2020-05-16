4677978931aeca2b14c1ebc3accd34822f6ecf72
leond1337 <58258111+leond1337@users.noreply.github.com>
2020-05-16 18:25:18 +0200

    Added normal flow showtimeselection

---
+++ CinemaProject/Model/Forms/MovieForm.cs
@@ -0,0 +1,56 @@
+﻿using CinemaProject.Model.Repository;
+using CinemaProject.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CinemaProject.Model.Forms
+{
+    public partial class MovieForm : Form
+    {
+        private readonly ShowTimeRepository _showTimeRepository;
+        private readonly MoviesRepository _moviesRepository;
+        private readonly List <ShowTimeView> MovieShowTimesList;
+        private readonly MovieView MovieDetails;
+        public MovieForm(int id_movie)
+        {
+            InitializeComponent();
+            _showTimeRepository = new ShowTimeRepository();
+            _moviesRepository = new MoviesRepository();
+            MovieShowTimesList = _showTimeRepository.GetAllShowTimesForMovie(id_movie);
+            MovieDetails = _moviesRepository.GetMovie(id_movie);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MovieForm_Load(object sender, EventArgs e)
+        {
+
+            label1.Text = MovieDetails.Name;
+            OmschrijvingMovie.Text = MovieDetails.Description;
+            pictureBox1.Load(MovieDetails.Picture);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            foreach (ShowTimeView i in MovieShowTimesList)
+            {
+                ShowtimeList.Items.Add(i.Id_MovieShowTime+ ": " + i.ShowTime);
+            }
+        }
+
+        private void ShowtimeList_DoubleClick(object sender, EventArgs e)
+        {
+            string selectedItem = ShowtimeList.Items[ShowtimeList.SelectedIndex].ToString();
+            var GetMovieShowtimeId = selectedItem.Split(':')[0];
+            var ReservationForm = new ReservationForm(int.Parse(GetMovieShowtimeId));
+            ReservationForm.Show();
+        }
+    }
+}
--- CinemaProject/Objects/MovieView.cs
+++ CinemaProject/Objects/MovieView.cs
@@ -12,17 +12,24 @@ namespace CinemaProject.View
         public string Name;
         public DateTime ReleaseDate;
         public string Genre;
+        public string Description;
+        public string Picture;
 
         public MovieView(
             int id_Movie,
             string name,
             string genre,
-            DateTime releaseDate)
+            DateTime releaseDate, 
+            string description,
+            string picture)
         {
             Id_Movie = id_Movie;
             Name = name;
             Genre = genre;
             ReleaseDate = releaseDate;
+            Description = description;
+            Picture = picture;
+
         }
     }
 }
